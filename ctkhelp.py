3e0d5e51252c5f40701eb3b115573a5c2b9830b0
unknown <sadjok>
2022-07-17 11:04:39 +0800

    Need Help 3.2.2 Commit (See the Update.txt)

--- ctkhelp.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-import customtkinter
-
-customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
-
-app = customtkinter.CTk()
-app.title("CustomTkinter Test")
-
-app.mainloop()
